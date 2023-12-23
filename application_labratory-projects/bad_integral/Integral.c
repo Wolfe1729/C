bf54aff3a9f9c93b3d281e84fc8a0fc3ee97776b
Alireza Ahmadi <AlirezaAhmadi1729@gmail.com>
2023-12-23 23:03:00 +0330

    Update Integral.c

--- application_labratory-projects/bad_integral/Integral.c
+++ application_labratory-projects/bad_integral/Integral.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 long double func(long double x )
 {
-    return x*(x*x);
+    return (x*x)*(x*x);
 }
 
 long double sum=0;
-void integral(float a, float b)
+void integral()//main func
 {
-long double g = a-b;
-    for (size_t i = 1; i < 999999999999999; i++)
+
+    for (size_t i = 1; i < 99999999999; i++)
     {   
 
         //printf ("%.18LF",func(i/300.0));
-        sum =sum + (( g/999999999999999.0 ) * func(i/999999999999999.0));
+        sum =sum + (( 1/99999999999.0 ) * func(i/99999999999.0));
     }
-    printf("\n%.18LF",sum);       
+    printf("\n%.100LF",sum);       
 }
 
 int main()//driver code
-{   float in_1,in_2;
-    scanf("%f%f",&in_1,&in_2);
-    integral(in_1,in_2);
+{   
+    integral();
     return 0;
 }
